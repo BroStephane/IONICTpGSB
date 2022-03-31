56acb8bc3aac888747f70bfedfffed2657a5428d
HILARICUS <HILARICUS@301P04>
2022-03-31 09:48:46 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gsb_ges_activite',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
